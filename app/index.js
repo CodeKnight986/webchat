871769580cdcab1e535e83642bcaebcba2491666
CodeKnight986 <rayche986@gmail.com>
2016-10-18 14:44:41 +0800

    fix

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
+const app = require('./web')
 
 function init(config) {
 	
-	const app = require('./web').init(config)
 	const io = require('./socket').init(app, config.port)
 	return {
 		app: app,
--- app/web/index.js
+++ app/web/index.js
@@ -7,33 +7,23 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
-function intApp(config) {
-	
-	app.engine('.tpl', exphbs({
-		defaultLayout: 'layout',
-		extname: '.tpl',
-		layoutsDir: path.join(__dirname),
-		partialsDir: path.join(__dirname),
-		helpers: {
-	        base_url: function () { return config.url; }
-	    }
-	}))
-	app.set('view engine', '.tpl')
-	app.set('views', path.join(__dirname))
+app.engine('.tpl', exphbs({
+	defaultLayout: 'layout',
+	extname: '.tpl',
+	layoutsDir: path.join(__dirname),
+	partialsDir: path.join(__dirname),
+}))
+app.set('view engine', '.tpl')
+app.set('views', path.join(__dirname))
 
 
-	app.use(express.static(path.join(__dirname, '../..', 'public')))
-	app.get("/", function(req, res) {
-		// res.send("It works!");
-		res.render('main')
-	})
-
-	return app
-
-}
+app.use(express.static(path.join(__dirname, '../..', 'public')))
+app.get("/", function(req, res) {
+	// res.send("It works!");
+	res.render('main')
+})
 
 
 
-module.exports = {
-	init: intApp
-}
+
+module.exports = app
